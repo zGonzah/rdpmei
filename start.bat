d4c5dddcb0c1974662287cfa91b096377cff9abf
akuh <81416541+akuhnet@users.noreply.github.com>
2021-05-01 17:24:19 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Akuh.net" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator @AkuhNetwork/add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo 6 Hours run out rebuild again
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed NGROK tunnel, NGROK_AUTH_TOKEN ivalid> Secrets> Repository secret : https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: @AkuhNetwork
+echo Login With RDP now
+ping -n 10 127.0.0.1 >nul
